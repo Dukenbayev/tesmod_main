9ea24c8247953937a1b6eae6938ec009be805fc8
Dukenbayev <marlen.dukenbaev@gmail.com>
2021-06-07 18:09:28 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mod
+
+go 1.16
+
+require github.com/Dukenbayev/testmod v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package tesmod_main
+
+import (
+	"fmt"
+
+	"github.com/Dukenbayev/testmod"
+)
+
+func main() {
+	fmt.Println(testmod.Hi("roberto"))
+}
